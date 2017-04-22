12c2c09d18357bf511d7871608fb709a3f9abe10
Albert Sheu <albert.sheu@gmail.com>
2017-04-21 22:21:29 -0700

    Make the title of the page editable

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -7,10 +7,35 @@ import './Page.css';
 
 export default class Page extends Component {
 
+  handleKeyDown = (e) => {
+    switch (e.key) {
+      case 'Enter':
+      case 'Esc':
+        e.preventDefault();
+        this.refs.title.blur();
+        break;
+      default:
+        break;
+    }
+  }
+
+  updateTitle = (e) => {
+    let newTitle = e.target.value;
+    this.props.store.updateTitle(newTitle);
+  }
+
   render() {
     return (
       <div className="Page">
-        <h1>{this.props.title}</h1>
+        <input
+          ref="title"
+          className="page-title"
+          onKeyDown={this.handleKeyDown}
+          onChange={this.updateTitle}
+          spellCheck={false}
+          value={this.props.title}
+          placeholder={'Untitled'}
+        />
         <ShuffleList {...this.props} />
         <UserSettings {...this.props} />
       </div>
